fde5918117e781d6446af4618464162cabc0aa76
Althaf <skalthaf586@gmail.com>
2022-10-11 21:05:01 +0530

    first commit

---
+++ characterCounter.js
@@ -0,0 +1,23 @@
+document.getElementById("remaining").innerHTML=text.getAttribute("maxlength");
+document.getElementById("text").addEventListener("input",function() {calculate(document.getElementById("text"))});
+function calculate(t)
+{
+
+ document.getElementById("total").innerHTML=t.value.length;
+ document.getElementById("remaining").innerHTML=text.getAttribute("maxlength")-document.getElementById("total").innerHTML;
+ if(t.value.length<10)
+ {
+  document.getElementById("total").style.color="black";
+  document.getElementById("remaining").style.color="black";
+ }
+  else if(t.value.length>=10 && t.value.length<30)
+ {
+  document.getElementById("total").style.color="orange";
+  document.getElementById("remaining").style.color="orange";
+ }
+   else if(t.value.length>=30)
+ {
+  document.getElementById("total").style.color="red";
+  document.getElementById("remaining").style.color="red";
+ }
+}
